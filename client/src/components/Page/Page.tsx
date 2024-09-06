1c516310bf16e2a835218af4fd4a3147999af390
Sofya <s.cheboltasova@g.nsu.ru>
2024-09-06 20:24:48 +0700

    client/feat: feat Page

---
+++ client/src/components/Page/Page.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import style from "./Page.module.scss";
+
+interface PageProps {
+  header: string;
+  subheader: string;
+  color: "white" | "black";
+  children?: React.ReactNode;
+}
+
+export default function Page(props: PageProps) {
+  const { header, subheader, color, children } = props;
+
+  return (
+    <section className={`${style.page} ${style[color]}`}>
+      <div className={style.page__text}>
+        <h2 className={style.header}>{header}</h2>
+        <div className={style.subheader__wrapper}>
+          <h3 className={style.subheader}>{subheader}</h3>
+        </div>
+      </div>
+
+      <div className={style.page__content}>{children}</div>
+    </section>
+  );
+}
+
