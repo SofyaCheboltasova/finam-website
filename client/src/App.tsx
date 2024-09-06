4574f540acdea87e87975919b942674f7d966564
Sofya <s.cheboltasova@g.nsu.ru>
2024-09-06 18:51:02 +0700

    client/refactor: update App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,13 @@
-import "./App.css";
+import style from "./App.module.scss";
+import WelcomePage from "./pages/WelcomePage/WelcomePage";
 
-function App() {}
+function App() {
+  return (
+    <div className={style.app}>
+      <WelcomePage />
+    </div>
+  );
+}
 
 export default App;
 
