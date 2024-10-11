018007b1695e63be6ea628806ccd5e50848be9e1
Sofya <s.cheboltasova@g.nsu.ru>
2024-10-11 19:40:18 +0700

    client/refactor: change styles

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -2,17 +2,18 @@ import style from "./Button.module.scss";
 
 interface ButtonProps {
   text: string;
+  href?: string;
   filled?: boolean;
 }
 
 export default function Button(props: ButtonProps) {
   return (
     <a
-      href="#"
+      href={props.href}
+      target="_blank"
       className={`${style.button} ${props.filled && style.button_filled}`}
     >
       <div>{props.text}</div>
     </a>
   );
 }
-
