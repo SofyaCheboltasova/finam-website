03f657f937087d7a8e5442e7391d658c0a8f8ba6
Sofya <s.cheboltasova@g.nsu.ru>
2024-10-13 03:12:49 +0700

    client/feat: create StrategiesPage

--- client/src/pages/StrategiesPage/StrategiesPage.tsx
+++ client/src/pages/StrategiesPage/StrategiesPage.tsx
@@ -1,8 +1,16 @@
+import { strategies } from "../../assets/data/strategiesData";
 import Page from "../../components/Page/Page";
+import { StrategyBlock } from "../../components/StrategyBlock/StrategyBlock";
 
 export default function StrategiesPage() {
   const header = "ГОТОВЫЕ СТРАТЕГИИ";
   const subheader = "Получайте высокую доходность с проверенными стратегиями";
-  return <Page header={header} subheader={subheader} color={"black"} />;
-}
 
+  return (
+    <Page header={header} subheader={subheader} color={"black"} sticky>
+      {strategies.map((strategy) => {
+        return <StrategyBlock data={strategy} />;
+      })}
+    </Page>
+  );
+}
