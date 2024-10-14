319b5d8a07c3522bc1c684a7ed9767b0d0cc630e
Sofya <s.cheboltasova@g.nsu.ru>
2024-10-14 14:22:12 +0700

    client/feat: create CasesPage

---
+++ client/src/pages/CasesPage/CasesPage.tsx
@@ -0,0 +1,12 @@
+import Page from "../../components/Page/Page";
+// import style from "./CasesPage.module.scss";
+
+export function CasesPage() {
+  return (
+    <Page
+      header={"Кейсы клиентов"}
+      subheader={"За 12 лет работы более 600 клиентов по республике Хакасия"}
+      color={"black"}
+    />
+  );
+}
