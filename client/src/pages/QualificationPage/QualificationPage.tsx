1e637fda6d6322dea8ee5162eca2418e0c562218
Sofya <s.cheboltasova@g.nsu.ru>
2024-10-14 14:20:12 +0700

    client/feat: create QualificationPage

--- client/src/pages/QualificationPage/QualificationPage.tsx
+++ client/src/pages/QualificationPage/QualificationPage.tsx
@@ -1,9 +1,41 @@
+import { InfiniteCarousel } from "../../components/InfiniteCarousel/InfiniteCarousel";
 import Page from "../../components/Page/Page";
+import style from "./QualificationPage.module.scss";
 
 export default function QualificationPage() {
   const header = "КВАЛИФИКАЦИЯ";
   const subheader =
     "Директор обладает 20-летним опытом практической работы на фондовом рынке, аттестованный специалист по рынку ценных бумаг:";
-  return <Page header={header} subheader={subheader} color={"white"} />;
-}
 
+  function getCarousel() {
+    const items = [];
+    for (let i = 1; i < 4; i++) {
+      const src = `public/images/docs/${i}.jpg`;
+      const className = style.image__rect;
+      const doc = <img src={src} alt="Document" className={className} />;
+      items.push(doc);
+    }
+    return <InfiniteCarousel items={items} />;
+  }
+
+  return (
+    <Page header={header} subheader={subheader} color={"white"}>
+      <div className={style.images}>
+        <div className={style.team}>
+          <img
+            src="public/images/team/1.jpg"
+            alt=""
+            className={style.img__square}
+          />
+          <img
+            src="public/images/team/2.jpg"
+            alt=""
+            className={style.img__square}
+          />
+        </div>
+
+        {getCarousel()}
+      </div>
+    </Page>
+  );
+}
