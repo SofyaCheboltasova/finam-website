9b084e304d547c23d3f02029241f555e74e81f59
Sofya <s.cheboltasova@g.nsu.ru>
2024-10-15 13:21:53 +0700

    client/fix: change logic of PartnersPage

--- client/src/pages/PartnersPage/PartnersPage.tsx
+++ client/src/pages/PartnersPage/PartnersPage.tsx
@@ -1,18 +1,40 @@
 import style from "./PartnersPage.module.scss";
 import Page from "../../components/Page/Page";
-import { companies } from "../../assets/data/companiesData";
-import { CompanyBlock } from "../../components/CompanyBlock/CompanyBlock";
+import { companies, points } from "../../assets/data/companiesData";
+import { InfiniteCarousel } from "../../components/InfiniteCarousel/InfiniteCarousel";
 
 export default function PartnersPage() {
   const header = "НАШИ ПАРТНЕРЫ";
   const subheader =
-    "С 2012 года Цитадель-Инвест является официальным представительством различных инвестиционных компаний";
+    "С 2012 года Цитадель-Инвест является официальным представительством ведущих инвестиционных компаний";
+
+  function getCarousel() {
+    const items: React.ReactNode[] = [];
+    companies.map((company) => {
+      const item = (
+        <div className={style.company}>
+          <img src={company.logo} alt="" />
+          {/* <div>{company.description}</div> */}
+        </div>
+      );
+      items.push(item);
+    });
+    return <InfiniteCarousel items={items} />;
+  }
+
   return (
-    <Page header={header} subheader={subheader} color={"white"}>
+    <Page id={`partners`} header={header} subheader={subheader} color={"white"}>
       <div className={style.partners}>
-        {companies.map((company) => (
-          <CompanyBlock key={company.id} data={company} />
-        ))}
+        {getCarousel()}
+        <ul className={style.description}>
+          <h3>В рамках сотрудничества мы предоставляем:</h3>
+
+          <div className={style.points}>
+            {points.map((point) => {
+              return <li className={style.point}>{point}</li>;
+            })}
+          </div>
+        </ul>
       </div>
     </Page>
   );
