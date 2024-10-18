be631b311652122d9be3f57ce1db571e91b4c5f6
Sofya <s.cheboltasova@g.nsu.ru>
2024-10-18 16:43:51 +0700

    client/refactor: change strategies page

--- client/src/pages/StrategiesPage/StrategiesPage.tsx
+++ client/src/pages/StrategiesPage/StrategiesPage.tsx
@@ -1,22 +1,43 @@
+import { useState } from "react";
 import { strategies } from "../../assets/data/strategiesData";
+import Button from "../../components/Button/Button";
 import Page from "../../components/Page/Page";
 import { StrategyBlock } from "../../components/StrategyBlock/StrategyBlock";
+import style from "./StrategiesPage.module.scss";
 
 export default function StrategiesPage() {
   const header = "ГОТОВЫЕ СТРАТЕГИИ";
   const subheader = "Получайте высокую доходность с проверенными стратегиями";
+  const risks = new Map<number, string>();
+  risks.set(1, "Консервативный");
+  risks.set(2, "Умеренный");
+  risks.set(3, "Агрессивный");
+
+  const [riskSelected, setRiskSelected] = useState<number>(1);
 
   return (
-    <Page
-      id="products"
-      color={"white"}
-      header={header}
-      subheader={subheader}
-      scrollType="horizontal"
-    >
-      {strategies.map((strategy) => {
-        return <StrategyBlock data={strategy} />;
-      })}
+    <Page id="products" color={"white"} header={header} subheader={subheader}>
+      <div className={style.selector}>
+        <span>Уровень риска:</span>
+        <div className={style.buttons}>
+          {[...risks.entries()].map(([id, name]) => (
+            <Button
+              text={name}
+              key={id}
+              filled={riskSelected === id}
+              onClick={() => setRiskSelected(id)}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className={style.strategies}>
+        {strategies
+          .filter((strategy) => strategy.risk === risks.get(riskSelected))
+          .map((strategy) => {
+            return <StrategyBlock data={strategy} />;
+          })}
+      </div>
     </Page>
   );
 }
