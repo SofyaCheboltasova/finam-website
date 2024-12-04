b2b4f6542daac778c29ebb4de12f5fe5b32f9508
s.cheboltasova <s.cheboltasova@vkteam.ru>
2024-12-04 20:13:09 +0700

    Feat mobile design

--- client/src/pages/PartnersPage/PartnersPage.tsx
+++ client/src/pages/PartnersPage/PartnersPage.tsx
@@ -1,53 +1,19 @@
 import style from "./PartnersPage.module.scss";
 import Page from "../../components/Page/Page";
 import { companies } from "../../assets/data/companiesData";
-import { InfiniteCarousel } from "../../components/InfiniteCarousel/InfiniteCarousel";
 import { CompanyBlock } from "../../components/CompanyBlock/CompanyBlock";
 
 export default function PartnersPage() {
   const header = "НАШИ ПАРТНЕРЫ";
   const subheader =
     "С 2012 года Цитадель-Инвест является официальным представительством в Хакасии ведущих инвестиционных компаний";
-
-  function getCarousel() {
-    const items: React.ReactNode[] = [];
-    companies.map((company) => {
-      const item = (
-        <div className={style.company}>
-          <img src={company.logo} alt="" />
-          {/* <div>{company.description}</div> */}
-        </div>
-      );
-      items.push(item);
-    });
-    return <InfiniteCarousel items={items} />;
-  }
-
   return (
     <Page id={`partners`} header={header} subheader={subheader} color={"white"}>
       <div className={style.partners}>
-        {/* {getCarousel()} */}
         <ul className={style.description}>
           {companies.map((company) => {
             return <CompanyBlock data={company} />;
           })}
-
-          {/* <div className={style.points_column}>
-              <h4>Для физических лиц:</h4>
-              <div className={style.points}>
-                {points.map((point) => {
-                  return <li className={style.point}>{point}</li>;
-                })}
-              </div>
-            </div>
-            <div className={style.points_column}>
-            <h4>Для юридических лиц:</h4>
-              <div className={style.points}>
-                {points.map((point) => {
-                  return <li className={style.point}>{point}</li>;
-                })}
-              </div>
-            </div> */}
         </ul>
       </div>
     </Page>
