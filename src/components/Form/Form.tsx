a5459ffc9588dbbaa61103557a5651b66d14d62e
s.cheboltasova <s.cheboltasova@vkteam.ru>
2024-12-05 02:32:02 +0700

    Feat request form, connect to tg api

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -10,6 +10,7 @@ export function Form(props: FormProps) {
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
     const [tel, setTel] = useState("");
+    const [isSent, setIsSent] = useState<boolean | null>(null);
 
     const [errors, setErrors] = useState({
         name: false,
@@ -23,7 +24,40 @@ export function Form(props: FormProps) {
         email: null as HTMLInputElement | null,
     });
 
-    function handleSubmit() {
+
+    async function fetchRequest() {
+        const text =
+            '<b>Новая заявка с сайта</b>\n' +
+            '- <b>Имя Фамилия:</b> ' + name + '\n' +
+            '- <b>Телефон:</b> ' + tel + '\n' +
+            '- <b>Email:</b> ' + email;
+
+        const url = `https://api.telegram.org/bot${import.meta.env.VITE_TG_BOT}/sendMessage`;
+        const messageData = {
+            chat_id: import.meta.env.VITE_CHAT_ID,
+            text: text,
+            parse_mode: 'HTML'
+        };
+
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(messageData),
+        });
+
+        if(response.ok) {
+            await response.json();
+            setIsSent(true);
+        }
+        else {
+            setIsSent(false)
+        }
+    }
+
+    function validate() {
+        setIsSent(null);
         const telRegExp = new RegExp(`^\\+7\\s?\\(?\\d{3}\\)?\\s?\\d{3}[-.\\s]?\\d{2}[-.\\s]?\\d{2}$|^8\\s?\\d{3}\\s?\\d{3}[-.\\s]?\\d{2}[-.\\s]?\\d{2}$`)
         const emailRegExp = new RegExp(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$`)
 
@@ -50,21 +84,27 @@ export function Form(props: FormProps) {
             setErrors((prev) => ({ ...prev, email: false }));
             ref.current.email.classList.remove(style.error);
         }
-        console.log(name, tel, email);
+    }
+
+    async function onSubmit() {
+        validate();
+        if((!errors.name && !errors.tel && !errors.email) && (name !== '' && email !== '' && tel !== '')) {
+            await fetchRequest()
+        }
     }
 
     return (
         <>
         <div className={style.background} onClick={() => props.setIsFormOpened(false)}> </div>
-            <form className={style.form__wrapper} onSubmit={handleSubmit}>
+            <form className={style.form__wrapper}>
             <h4>Свяжитесь с нами</h4>
             <div className={style.socials}>
                 <Button text={'WhatsApp'} href={'https://wa.me/79134489089'} />
                 <Button text={'Telegram'} href={'https://t.me/+79134489089'}/>
-                <Button text={'Звонок'} href={'"tel:79134489089"'}/>
+                <Button text={'Звонок'} href={'tel:79134489089'}/>
             </div>
 
-            <h4> или оставьте заявку на консультацию</h4>
+            <h4>или оставьте заявку на консультацию</h4>
                 <div className={style.inputs}>
                     <input
                         ref={(el) => (ref.current.name = el)}
@@ -75,7 +115,7 @@ export function Form(props: FormProps) {
                         required
                         value={name}
                         onChange={(e) => setName(e.target.value)}
-                        onBlur={handleSubmit}
+                        onBlur={validate}
                     />
                     {errors.name && <span className={style.error_text}>Введите корректные данные</span>}
                     <input
@@ -87,7 +127,7 @@ export function Form(props: FormProps) {
                         required
                         value={tel}
                         onChange={(e) => setTel(e.target.value)}
-                        onBlur={handleSubmit}
+                        onBlur={validate}
                     />
                     {errors.tel && <span className={style.error_text}>Формат: +7 (XXX) XXX-XX-XX или 8XXXXXXXXXX</span>}
                     <input
@@ -98,13 +138,14 @@ export function Form(props: FormProps) {
                         placeholder={'user@example.com'}
                         onChange={(e) => setEmail(e.target.value)}
                         className={style.input}
-                        onBlur={handleSubmit}
+                        onBlur={validate}
                     />
                     {errors.email && <span className={style.error_text}>Формат: user@example.com</span>}
                 </div>
 
-
-                <Button text={'Отправить'} onClick={handleSubmit}/>
+                {isSent && <span className={style.sent_ok}>Скоро с вами свяжется наш специалист!</span>}
+                {isSent === false && <span className={style.error_text}>Ошибка! Повторите попытку</span>}
+                <Button text={'Отправить'} onClick={onSubmit}/>
             </form>
         </>
     )
